1b3dee3a52466f5d3c93d04c9b180051f28ada9e
Hector Piteau <hector.piteau@gmail.com>
2023-04-14 15:33:18 +0200

    Add forward pass for the volume rendering algorithm.
    Signed-off-by: Hector Piteau <hector.piteau@gmail.com>

--- src/cuda/VolumeRendering.cuh
+++ src/cuda/VolumeRendering.cuh
@@ -4,10 +4,36 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <surface_functions.h>
+#include <glm/glm.hpp>
+
+using namespace glm;
+
+struct Ray
+{
+    vec3 origin;
+    vec3 dir;
+    float tmin, tmax;
+
+};
+
+struct VolumeData
+{
+    float4 data;
+};
+
+
+
+class RayCaster {
+public:
+    struct Ray GetRay(){
+        struct Ray ray = {};
+        return ray;
+    }
+};
 
 // Forward declaration of CUDA render
 extern "C" 
-void volume_rendering_wrapper(cudaSurfaceObject_t& surf3d, size_t width, size_t height, size_t depth);
+void volume_rendering_wrapper(RayCaster& rayCaster, cudaTextureObject_t &volume, float4* outTexture, size_t width, size_t height);
 
-
+// void volume_rendering_wrapper(cudaTextureObject_t& volume, cudaSurfaceObject_t& outTexture, size_t width, size_t height, size_t depth);
 #endif //KERNEL_H
