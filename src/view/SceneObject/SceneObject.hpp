9f4fba1cc31cae15223d28bee7d4cb2d7ce249e1
Hector Piteau <hector.piteau@gmail.com>
2023-04-20 17:36:26 +0200

    Modify the SceneObject interface render method.
    Signed-off-by: Hector Piteau <hector.piteau@gmail.com>

--- src/view/SceneObject/SceneObject.hpp
+++ src/view/SceneObject/SceneObject.hpp
@@ -12,6 +12,10 @@ enum SceneObjectTypes {
     VOLUME3D,
     NERFDATASET,
     CAMERASET,
+    GIZMO,
+    MODEL,
+    MESH,
+    VOLUMERENDERER,
     NONE
 };
 
@@ -22,12 +26,18 @@ static const std::vector<const char*> SceneObjectNames = {
     "LineGrid",
     "Volume3D",
     "NeRF Dataset",
+    "Camera Set",
+    "Gizmo",
+    "Model",
+    "Mesh",
+    "Volume Renderer",
+    "None"
 };
 
 class SceneObject : public Renderable{
 public:
     
-    SceneObject(const std::string typeName, enum SceneObjectTypes type) : m_typeName(typeName), m_type(type), m_id(-1){}
+    SceneObject(const std::string typeName, enum SceneObjectTypes type) : m_typeName(typeName), m_type(type), m_id(-1), m_listVisible(true) {}
 
     /**
      * @brief Get the uniq-id that identify this object.
@@ -90,18 +100,33 @@ public:
      * @return const std::string& : A constant string that contains the name of the type used. 
      */
     enum SceneObjectTypes GetType(){ return m_type; }
-
-    /** Remove copy constructor. */
-    SceneObject(const SceneObject&) = delete;
     
     /** Default constructor. Create an invalid SceneObject. */
     SceneObject() : m_id(-1){}
 
     std::vector<std::shared_ptr<SceneObject>>& GetChildren(){ return m_children;}
+    
+    std::shared_ptr<SceneObject> GetChild(int id) {
+        if(m_children.size() > 0){
+            for(auto child : m_children){
+                if(child->GetID() == id){
+                    return child; 
+                }else{
+                   auto object_in_child = child->GetChild(id);
+                   if(object_in_child != nullptr) return object_in_child; 
+                }
+            }
+        }
+
+        return nullptr;
+    }
 
     bool IsChild(){ return m_isChild;}
 
     void SetIsChild(bool isChild){m_isChild = isChild;}
+
+    void SetIsVisibleInList(bool visible) { m_listVisible = visible;}
+    bool IsVisibleInList(){return m_listVisible;}
 
 protected:
     bool m_isChild;
@@ -109,6 +134,7 @@ protected:
     int m_id;
     /** True if the object is active in the scene (visible) or not. */
     bool m_active;
+    bool m_listVisible;
     /** The Object's name. */
     std::string m_name;
     /** The Object's type, stored as a string in order to be easily extended. */
