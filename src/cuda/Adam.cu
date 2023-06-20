19f4bf654b0abfb5e093f710f21fea98aa7cc02d
hepiteau <hector.piteau@gmail.com>
2023-06-21 01:36:36 +0200

    adam update wip.
    Signed-off-by: hepiteau <hector.piteau@gmail.com>

---
+++ src/cuda/Adam.cu
@@ -0,0 +1,5 @@
+//
+// Created by hepiteau on 21/06/23.
+//
+
+#include "Adam.cuh"
---
+++ src/cuda/Adam.cuh
@@ -0,0 +1,14 @@
+//
+// Created by hepiteau on 21/06/23.
+//
+
+#ifndef DRTMCS_ADAM_CUH
+#define DRTMCS_ADAM_CUH
+
+
+class Adam {
+
+};
+
+
+#endif //DRTMCS_ADAM_CUH
